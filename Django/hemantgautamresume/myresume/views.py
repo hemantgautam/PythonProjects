9bef323da0e164c62113ba334d8f2c21b60784ef
Hemant <hemant.a.gautam@accenture.com>
2020-03-24 18:56:09 +0530

    Updated Index.html

--- Django/hemantgautamresume/myresume/views.py
+++ Django/hemantgautamresume/myresume/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def myresume(request):
-    pro_skills = {"Python 3, Automation": "75",
-                  "Web Scraping, Django": "70",
+    pro_skills = {"Python3, Django, Rest Framework": "75",
+                  "Python Web Scraping ": "70",
                   "PHP, Drupal 6,7,8": "70",
-                  "Acquia, Pantheon, Bit Bucket, GIT, Heroku": "80",
-                  "MySQL, MongoDB, PostgreSQL": "70",
-                  "CSS, HTML, jQuery, Webservices": "75"}
+                  "Acquia, Pantheon, Bit Bucket, GIT, SVN, Heroku": "80",
+                  "SQLite, MySQL, MongoDB, PostgreSQL": "60",
+                  "CSS, HTML, jQuery, Bootstrap": "75"}
 
     personal_skills = {"Leadership ": "85",
                        "Proactive Team Member": "80",
@@ -16,6 +16,6 @@ def myresume(request):
                        "Good at Problem Solving": "90",
                        "Ability to Work Under Pressure": "95"}
 
-    interests = ['Fitness Freak', 'Guitar', 'Swimming', 'Squash', 'Freelancer', 'Travelling']
+    interests = ['Fitness Freak', 'Guitar', 'Swimming', 'Squash', 'Travelling']
     return render(request, 'index.html',
                   {'pro_skills': pro_skills, 'personal_skills': personal_skills, 'interests': interests})
